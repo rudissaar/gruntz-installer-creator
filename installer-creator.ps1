eb195918ab0fb2195b778823ad66683a01d27cdf
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-01-04 15:14:00 +0000

    Development | Refactored functions.

--- installer-creator.ps1
+++ installer-creator.ps1
@@ -26,16 +26,46 @@ Set-StrictMode -Version 3
 
 function Main
 {
-    Get-7zip
-    Test-Media $Media
+    if ((Get-7zip) -eq 1) {
+        Write-Output "> Unable to find 7z.exe from your environment's PATH variable."
+        Write-Output '> Aborting.'
+        exit(1)
+    }
+
+    Write-Output "> 7zip binary found at: '$(Get-7zip)'"
+
+    if (-Not ((Test-Media $Media) -eq 0)) {
+        if ((Test-Media $Media) -eq 1) {
+            Write-Output "> Specified ISO file doesn't exist on your filesystem."
+        }
+
+        if ((Test-Media $Media) -eq 2) {
+            Write-Output "> Specified ISO file doesn't match with required fingerprint."
+        }
+
+        Write-Output '> Aborting.'
+        exit(1)
+    }
+
+    if ((Create-Directory 'packages/eu.murda.gruntz/data') -eq 0) {
+        Write-Output "> Created directory: 'packages/eu.murda.gruntz/data'."
+    }
+}
+
+function Create-Directory ([string] $Directory)
+{
+    if (-Not (Test-Path -PathType Container $Directory)) {
+        [void] (New-Item -Path $Directory -ItemType Directory)
+        return 0
+    }
+
+    return 1
 }
 
 Function Get-7zip
 {
     if (Get-Command '7z.exe' -ErrorAction SilentlyContinue) {
         return (Get-Command '7z.exe' | Select -ExpandProperty Source)
-    } else {
-        Write-Output "> Unable to find 7z.exe from your environment's PATH variable."
     }
 
     return 1
@@ -48,15 +78,13 @@ Function Test-Media ([string] $Path)
     )
 
     if (-Not (Test-Path -PathType Leaf $Path)) {
-        Write-Output "> Specified file doesn't exist on your filesystem."
         return 1
     }
 
     $Hash = Get-FileHash -Algorithm MD5 $Path | Select -ExpandProperty Hash
 
     if (-Not ($ValidHashes.Contains($Hash))) {
-        Write-Output "> Specified file doesn't match with required fingerprint."
-        return 1
+        return 2
     }
 
     return 0
